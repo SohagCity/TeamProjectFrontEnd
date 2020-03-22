064c5a7a81c996a04e86e883201684799f6f4ead
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-03-22 18:24:33 +0100

    Change to the API URL

--- src/misc/backend.js
+++ src/misc/backend.js
@@ -1,1 +1,1 @@
-export default process.env.NODE_ENV === 'production' ? 'http://localhost:8000' : 'https://group6-backend.herokuapp.com'
+export default process.env.NODE_ENV === 'production' ? 'https://group6-backend.herokuapp.com' : 'http://localhost:8000'
