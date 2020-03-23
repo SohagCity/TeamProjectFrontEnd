d3b7de9d1cb203bec5362ec13b9cbb2da87a886e
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-03-24 00:00:02 +0100

    Small fixes and added logo

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../App.scss';
+import TeamPic from '../assets/Teampic.png'
 
-
+// Landing page for the app (the default website which opens upon first visit)
 function LandingPage() {
   return(
     <div>
         <h1>Welcome to the ATS System for "AirVia"</h1>
-        <h1>Please use the secure log in to get access</h1>
+        <h1>Please sign in to get access</h1>
+        <br/> <br/>
+        <h3> System brought to you by </h3>
+        <img alt='Team 6ix' style={{ width: '200px' }} src={TeamPic}/>
     </div>
 
   );
